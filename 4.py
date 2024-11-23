333a24d0c9ddd4835a4206b687e45301442535be
RedLadeR <kuznetsovroman775@gmail.com>
2024-11-23 16:55:17 +0200

    added 4.py

---
+++ 4.py
@@ -0,0 +1,21 @@
+class Car:
+
+    def __init__(self, name, hunger = 0, boredom =0):
+        self.name = name
+        self.hunger = hunger
+        self.boredom = boredom
+    
+    def talk(self):
+        print("Wroom Wroom", self.name)
+
+    def __str__(self):
+        ans = "Object class Car\n"
+        ans += "Country: " + self.name + "\n"
+        return ans
+
+def main():
+    car1 = Car("Japan")
+    car2 = Car("USA")
+    print(car2)
+
+main()
